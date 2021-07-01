b031a7247cd9fbf666a081a20b24d48877b7e23b
Cytown <cytown@gmail.com>
2021-07-01 19:41:44 +0800

    update version info

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -42,7 +42,7 @@
  * version was tagged.
  */
 //#define STRING_DISTRIBUTION_DATE "2021-06-27"
-#define STRING_DISTRIBUTION_DATE "2021-06-30"
+#define STRING_DISTRIBUTION_DATE "2021-07-01"
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
